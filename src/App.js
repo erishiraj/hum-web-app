8a508701f889d3f1a80361b8ce855ecb59112d26
erishiraj <erishiraj@gmail.com>
2021-10-08 00:49:57 +0530

    app created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+// Custom
+import Autocomplete from "./components/AutoComplete";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="app-header">React Autocomplete Project</h1>
+      <h2 className="app-description">Type your text and get your result</h2>
+      <Autocomplete
+        suggestions={[
+          "Angular",
+          "Blitzjs",
+          "Gatsby",
+          "Reactjs",
+          "Vuejs",
+          "Svelte",
+          "Nextjs",
+          "Node",
+          "Express",
+          "Sails",
+          "Loopback",
+          "React-router",
+          "Redux",
+          "Flux",
+          "Yarn",
+          "Npm",
+        ]}
+      />
     </div>
   );
 }
